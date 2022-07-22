7b627641d34aa5513609523bc5f957c3d852e344
Alejandro Durán <alejosandu@gmail.com>
2022-07-22 00:13:27 +0000

    Finish caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -11,6 +11,13 @@ from blog.api.serializers import (
 from blog.models import Post, Tag
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
+# CACHE
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
+
+from rest_framework.exceptions import PermissionDenied
+
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -30,9 +37,36 @@ class PostViewSet(viewsets.ModelViewSet):
             return PostSerializer
         return PostDetailSerializer
 
+    @method_decorator(cache_page(120))
+    def list(self, *args, **kwargs):
+        return super(PostViewSet, self).list(*args, **kwargs)
+
+    @method_decorator(cache_page(300))
+    def get(self, *args, **kwargs):
+        return super(UserDetail, self).get(*args, *kwargs)
+
+    @method_decorator(cache_page(300))
+    @method_decorator(vary_on_headers("Authorization", "Cookie"))
+    # @method_decorator(vary_on_cookie) SAME AS @vary_on_headers("Cookie")
+    @action(methods=["get"], detail=False, name="Posts by the logged in user")
+    def mine(self, request):
+        if request.user.is_anonymous:
+            raise PermissionDenied("You must be logged in to see which Posts are yours")
+        posts = self.get_queryset().filter(author=request.user)
+        serializer = PostSerializer(posts, many=True, context={"request": request})
+        return Response(serializer.data)
+
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
+    @method_decorator(cache_page(300))
+    def list(self, *args, **kwargs):
+        return super(TagViewSet, self).list(*args, **kwargs)
+
+    @method_decorator(cache_page(300))
+    def retrieve(self, *args, **kwargs):
+        return super(TagViewSet, self).retrieve(*args, **kwargs)
 
     @action(methods=["get"], detail=True, name="Posts with the Tag")
     def posts(self, request, pk=None):
