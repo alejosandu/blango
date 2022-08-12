ea171aea800020cc277003da8669ca3036b33357
Alejandro Durán <alejosandu@gmail.com>
2022-08-12 03:03:01 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
@@ -42,4 +43,6 @@ def get_ip(request):
 
 # render javascript
 def post_table(request):
-    return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
